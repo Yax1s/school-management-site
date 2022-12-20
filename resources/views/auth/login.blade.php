397c73af7615ae575cbafbf6ecef2c22c9ea82bb
yax1s <jeff.kiilu@strathmore.edu>
2022-12-20 18:05:33 +0300

    sign up link color

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,29 +9,29 @@
     <link rel="icon" href="../images/favicon.ico">
 
     <title>Easy ERP - Log in </title>
-  
+
     <!-- Vendors Style-->
     <link rel="stylesheet" href="{{ asset('backend/css/vendors_css.css') }}">
-      
-    <!-- Style-->  
+
+    <!-- Style-->
     <link rel="stylesheet" href="{{ asset('backend/css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('backend/css/skin_color.css') }}">   
+    <link rel="stylesheet" href="{{ asset('backend/css/skin_color.css') }}">
 
 </head>
 <body class="hold-transition theme-primary bg-gradient-primary">
-    
+
     <div class="container h-p100">
-        <div class="row align-items-center justify-content-md-center h-p100">   
-            
+        <div class="row align-items-center justify-content-md-center h-p100">
+
             <div class="col-12">
                 <div class="row justify-content-center no-gutters">
                     <div class="col-lg-4 col-md-5 col-12">
                         <div class="content-top-agile p-10">
                             <h2 class="text-white">Get started with Us</h2>
-                            <p class="text-white-50">Sign in to start your session</p>                          
+                            <p class="text-white-50">Sign in to start your session</p>
                         </div>
                         <div class="p-30 rounded30 box-shadowed b-2 b-dashed">
-   
+
      <form method="POST" action="{{ route('login') }}">
             @csrf
 
@@ -70,7 +70,7 @@
             </div>
             <!-- /.col -->
           </div>
-    </form>                                                     
+    </form>
 
     <div class="text-center text-white">
       <p class="mt-20">- Sign With -</p>
@@ -79,11 +79,11 @@
           <a class="btn btn-social-icon btn-round btn-outline btn-white" href="#"><i class="fa fa-twitter"></i></a>
           <a class="btn btn-social-icon btn-round btn-outline btn-white" href="#"><i class="fa fa-google-plus"></i></a>
           <a class="btn btn-social-icon btn-round btn-outline btn-white" href="#"><i class="fa fa-instagram"></i></a>
-        </p>    
+        </p>
     </div>
-    
+
     <div class="text-center">
-        <p class="mt-15 mb-0 text-white">Don't have an account? <a href="{{ route('register') }}" class="text-info ml-5">Sign Up</a></p>
+        <p class="mt-15 mb-0 text-white">Don't have an account? </p> <a href="{{ route('register') }}"  style="color:greenyellow">Sign Up</a>
                             </div>
                         </div>
                     </div>
@@ -95,7 +95,7 @@
 
     <!-- Vendor JS -->
     <script src="{{ asset('backend/js/vendors.min.js') }}"></script>
-    <script src="{{ asset('../assets/icons/feather-icons/feather.min.js') }}"></script>    
+    <script src="{{ asset('../assets/icons/feather-icons/feather.min.js') }}"></script>
 
 </body>
 </html>
