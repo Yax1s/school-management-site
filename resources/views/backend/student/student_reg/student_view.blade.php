67782ee6c4e08d8e7113f162e4986486f0230739
yax1s <jeff.kiilu@strathmore.edu>
2022-12-20 14:27:24 +0300

    student registration changes

--- resources/views/backend/student/student_reg/student_view.blade.php
+++ resources/views/backend/student/student_reg/student_view.blade.php
@@ -5,12 +5,12 @@
  <div class="content-wrapper">
 	  <div class="container-full">
 		<!-- Content Header (Page header) -->
-		 
+
 
 		<!-- Main content -->
 		<section class="content">
 		  <div class="row">
-		
+
 <div class="col-12">
 <div class="box bb-3 border-warning">
 				  <div class="box-header">
@@ -18,9 +18,9 @@
 				  </div>
 
 				  <div class="box-body">
-				
+
 		<form method="GET" action="{{ route('student.year.class.wise') }}">
-			
+
 			<div class="row">
 
 
@@ -35,16 +35,16 @@
 			 @foreach($years as $year)
  <option value="{{ $year->id }}" {{ (@$year_id == $year->id)? "selected":"" }} >{{ $year->name }}</option>
 		 	@endforeach
-			 
+
 		</select>
-	  </div>		 
 	  </div>
-	  
- 			</div> <!-- End Col md 4 --> 
+	  </div>
+
+ 			</div> <!-- End Col md 4 -->
 
 
 
- 			
+
  		<div class="col-md-4">
 
  		 <div class="form-group">
@@ -55,52 +55,48 @@
 			 @foreach($classes as $class)
 			<option value="{{ $class->id }}" {{ (@$class_id == $class->id)? "selected":"" }}>{{ $class->name }}</option>
 		 	@endforeach
-			 
+
 		</select>
-	  </div>		 
 	  </div>
-	  
- 			</div> <!-- End Col md 4 --> 
+	  </div>
+
+ 			</div> <!-- End Col md 4 -->
 
 
  			<div class="col-md-4" style="padding-top: 25px;">
 
- <input type="submit" class="btn btn-rounded btn-dark mb-5" name="search" value="Search">
-	  
- 			</div> <!-- End Col md 4 --> 
+ <input type="submit" class="btn btn-rounded btn-dark mb-5" name="<?php const MSG="search";?>" value="Search">
 
- 
+ 			</div> <!-- End Col md 4 -->
 
-				
-			</div><!--  end row --> 
+			</div><!--  end row -->
 
 		</form>
- 
-					 
+
 				  </div>
 				</div>
 	</div> <!-- // end first col 12 -->
 
 
-			 
+
 
 			<div class="col-12">
 
 			 <div class="box">
 				<div class="box-header with-border">
 				  <h3 class="box-title">Student List</h3>
-	<a href="{{ route('student.registration.add') }}" style="float: right;" class="btn btn-rounded btn-success mb-5"> Add Student  </a>			  
+	<a href="{{ route('student.registration.add') }}" style="float: right;" class="btn btn-rounded btn-success mb-5"> Add Student  </a>
 
 				</div>
 				<!-- /.box-header -->
 				<div class="box-body">
 					<div class="table-responsive">
 
-	@if(!@search)					
+	@if(!@MSG)
 	 <table id="example1" class="table table-bordered table-striped">
 						<thead>
 			<tr>
-				<th width="5%">SL</th>  
+				<th width="5%">SL</th>
 				<th>Name</th>
 				<th>ID No</th>
 				<th>Roll</th>
@@ -111,7 +107,7 @@
 				<th>Code</th>
 				 @endif
 				<th width="25%">Action</th>
-				 
+
 			</tr>
 		</thead>
 		<tbody>
@@ -119,14 +115,14 @@
 			<tr>
 				<td>{{ $key+1 }}</td>
 				<td> {{ $value['student']['name'] }}</td>
-				<td> {{ $value['student']['id_no'] }}</td>	
-				<td> {{ $value->roll }}  </td>	
-				<td> {{ $value['student_year']['name'] }}</td>	
-				<td>  {{ $value['student_class']['name'] }}</td>	
+				<td> {{ $value['student']['id_no'] }}</td>
+				<td> {{ $value->roll }}  </td>
+				<td> {{ $value['student_year']['name'] }}</td>
+				<td>  {{ $value['student_class']['name'] }}</td>
 				<td>
-	 <img src="{{ (!empty($value['student']['image']))? url('upload/student_images/'.$value['student']['image']):url('upload/no_image.jpg') }}" style="width: 60px; width: 60px;"> 
-				</td>	
-				<td> {{ $value->year_id }}</td>				 
+	 <img src="{{ (!empty($value['student']['image']))? url('upload/student_images/'.$value['student']['image']):url('upload/no_image.jpg') }}" style="width: 60px; width: 60px;">
+				</td>
+				<td> {{ $value->year_id }}</td>
 				<td>
 <a title="Edit" href="{{ route('student.registration.edit',$value->student_id) }}" class="btn btn-info"> <i class="fa fa-edit"></i> </a>
 
@@ -135,13 +131,13 @@
 <a target="_blank" title="Details" href="{{ route('student.registration.details',$value->student_id) }}" class="btn btn-danger"  ><i class="fa fa-eye"></i></a>
 
 				</td>
-				 
+
 			</tr>
 			@endforeach
-							 
+
 						</tbody>
 						<tfoot>
-							 
+
 						</tfoot>
 					  </table>
 
@@ -150,7 +146,7 @@
 	  <table id="example1" class="table table-bordered table-striped">
 						<thead>
 			<tr>
-				<th width="5%">SL</th>  
+				<th width="5%">SL</th>
 				<th>Name</th>
 				<th>ID No</th>
 				<th>Roll</th>
@@ -161,7 +157,7 @@
 				<th>Code</th>
 				 @endif
 				<th width="25%">Action</th>
-				 
+
 			</tr>
 		</thead>
 		<tbody>
@@ -169,14 +165,14 @@
 			<tr>
 				<td>{{ $key+1 }}</td>
 				<td> {{ $value['student']['name'] }}</td>
-				<td> {{ $value['student']['id_no'] }}</td>	
-				<td> {{ $value->roll }}  </td>	
-				<td> {{ $value['student_year']['name'] }}</td>	
-				<td>  {{ $value['student_class']['name'] }}</td>	
+				<td> {{ $value['student']['id_no'] }}</td>
+				<td> {{ $value->roll }}  </td>
+				<td> {{ $value['student_year']['name'] }}</td>
+				<td>  {{ $value['student_class']['name'] }}</td>
 				<td>
-	 <img src="{{ (!empty($value['student']['image']))? url('upload/student_images/'.$value['student']['image']):url('upload/no_image.jpg') }}" style="width: 60px; width: 60px;"> 
-				</td>	
-				<td> {{ $value->year_id }}</td>				 
+	 <img src="{{ (!empty($value['student']['image']))? url('upload/student_images/'.$value['student']['image']):url('upload/no_image.jpg') }}" style="width: 60px; width: 60px;">
+				</td>
+				<td> {{ $value->year_id }}</td>
 				<td>
 <a title="Edit" href="{{ route('student.registration.edit',$value->student_id) }}" class="btn btn-info"> <i class="fa fa-edit"></i> </a>
 
@@ -185,18 +181,18 @@
 <a target="_blank" title="Details" href="{{ route('student.registration.details',$value->student_id) }}" class="btn btn-danger"  ><i class="fa fa-eye"></i></a>
 
 				</td>
-				 
+
 			</tr>
 			@endforeach
-							 
+
 						</tbody>
 						<tfoot>
-							 
+
 						</tfoot>
 					  </table>
 
 
-			@endif
+		@endif
 
 
 
@@ -206,14 +202,14 @@
 			  </div>
 			  <!-- /.box -->
 
-			       
+
 			</div>
 			<!-- /.col -->
 		  </div>
 		  <!-- /.row -->
 		</section>
 		<!-- /.content -->
-	  
+
 	  </div>
   </div>
 
