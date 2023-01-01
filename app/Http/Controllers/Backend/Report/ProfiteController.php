d2ca355ef015afd67a4eeca83bfca653684de5a9
yax1s <jeff.kiilu@strathmore.edu>
2023-01-01 20:19:37 +0300

    Spelling mistakes

--- app/Http/Controllers/Backend/Report/ProfiteController.php
+++ app/Http/Controllers/Backend/Report/ProfiteController.php
@@ -11,7 +11,7 @@ use App\Models\AccountStudentFee;
 
 use PDF;
 
-class ProfiteController extends Controller
+class ProfitController extends Controller
 {
     public function MonthlyProfitView(){
     	return view('backend.report.profit.profit_view');
@@ -26,16 +26,16 @@ class ProfiteController extends Controller
 		$end_date = date('Y-m',strtotime($request->end_date));
     	$sdate = date('Y-m-d',strtotime($request->start_date));
     	$edate = date('Y-m-d',strtotime($request->end_date));
-    	 
+
     	$student_fee = AccountStudentFee::whereBetween('date',[$start_date,$end_date])->sum('amount');
 
-    	$other_cost = AccountOtherCost::whereBetween('date',[$sdate,$edate])->sum('amount'); 
+    	$other_cost = AccountOtherCost::whereBetween('date',[$sdate,$edate])->sum('amount');
 
     	$emp_salary = AccountEmployeeSalary::whereBetween('date',[$start_date,$end_date])->sum('amount');
 
     	$total_cost = $other_cost+$emp_salary;
-    	$profit = $student_fee-$total_cost;  
-    	  
+    	$profit = $student_fee-$total_cost;
+
     	 $html['thsource']  = '<th>Student Fee</th>';
     	 $html['thsource'] .= '<th>Other Cost</th>';
     	 $html['thsource'] .= '<th>Employee Salary</th>';
@@ -52,10 +52,10 @@ class ProfiteController extends Controller
     	 $html['tdsource'] .='<td>';
     	 	$html['tdsource'] .='<a class="btn btn-sm btn-'.$color.'" title="PDF" target="_blanks" href="'.route("report.profit.pdf").'?start_date='.$sdate.'&end_date='.$edate.'">Pay Slip</a>';
     	 	$html['tdsource'] .= '</td>';
-    	
-    	return response()->json(@$html); 
 
-    } // end method 
+    	return response()->json(@$html);
+
+    } // end method
 
 
     public function MonthlyProfitPdf(Request $request){
@@ -74,4 +74,3 @@ class ProfiteController extends Controller
 
 
 }
- 
--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,7 @@ use App\Http\Controllers\Backend\Account\StudentFeeController;
 use App\Http\Controllers\Backend\Account\AccountSalaryController;
 use App\Http\Controllers\Backend\Account\OtherCostController;
 
-use App\Http\Controllers\Backend\Report\ProfiteController;
+use App\Http\Controllers\Backend\Report\ProfitController;
 use App\Http\Controllers\Backend\Report\MarkSheetController;
 use App\Http\Controllers\Backend\Report\AttenReportController;
 use App\Http\Controllers\Backend\Report\ResultReportController;
@@ -451,11 +451,11 @@ Route::post('other/cost/update/{id}', [OtherCostController::class, 'OtherCostUpd
 /// Report Management All Routes
 Route::prefix('reports')->group(function(){
 
-Route::get('monthly/profit/view', [ProfiteController::class, 'MonthlyProfitView'])->name('monthly.profit.view');
+Route::get('monthly/profit/view', [ProfitController::class, 'MonthlyProfitView'])->name('monthly.profit.view');
 
-Route::get('monthly/profit/datewais', [ProfiteController::class, 'MonthlyProfitDatewais'])->name('report.profit.datewais.get');
+Route::get('monthly/profit/datewais', [ProfitController::class, 'MonthlyProfitDatewais'])->name('report.profit.datewais.get');
 
-Route::get('monthly/profit/pdf', [ProfiteController::class, 'MonthlyProfitPdf'])->name('report.profit.pdf');
+Route::get('monthly/profit/pdf', [ProfitController::class, 'MonthlyProfitPdf'])->name('report.profit.pdf');
 
 // MarkSheet Generate Routes
 Route::get('marksheet/generate/view', [MarkSheetController::class, 'MarkSheetView'])->name('marksheet.generate.view');
